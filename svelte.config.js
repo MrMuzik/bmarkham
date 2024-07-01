3df0ff1751a362210a82c06e201e209d9832ff58
Bryan Markham <muzik702@gmail.com>
2024-07-01 12:31:11 -0400

    cleaning working build

--- svelte.config.js
+++ svelte.config.js
@@ -17,13 +17,7 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: 'index.html'
-		  }),
-		// prerender: {
-		// 	default: true
-		// }
-		// paths: {
-		// 	base: process.env.NODE_ENV === 'production' ? '/bmarkham' : ''
-		// }
+		  })
 	}
 };
 
